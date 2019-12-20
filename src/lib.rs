523411aa6359d1f83eb47e354538964db271abd2
Andrew Grangaard <granny-github@ofb.net>
2019-12-20 16:02:18 -0500

    Initial Commit: find a crate's Cargo.toml and return as PathBuf

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+use std::env::current_dir;
+use std::path::{Path,PathBuf};
+use std::io;
+
+
+pub fn find_config_toml<P: Into<PathBuf>>(path: Option<P>) -> Result<(PathBuf),(io::Error)>{
+    //! Walks upward from `path` or `current_dir()` looking for the first `Cargo.toml` file.
+    let path = match path {
+        Some(p) => p.into(),
+        None => current_dir()?
+    };
+
+    println!("path is {:?}", path);
+
+    find_file_upwards(path, "Cargo.toml")
+}
+
+pub fn find_file_upwards<P: Into<PathBuf>, S: AsRef<Path>>(path: P, file_name: S) -> Result<(PathBuf),(io::Error)>{
+    //! Walks upward from `path` looking for the first file named `file_name`.
+    let path = path.into();
+
+    for current in path.ancestors() {
+        let manifest = current.join(&file_name);
+        if manifest.is_file() {
+            return Ok(manifest)
+        }
+    }
+    Err(io::Error::new(io::ErrorKind::NotFound,"Manifest Not Found, not a rust package"))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{find_config_toml,find_file_upwards};
+    use std::env::current_dir;
+
+    #[test]
+    fn test_find_file_upwards() {
+        // look for src/lib.rs
+        let src = current_dir().expect("expected a working current_directory").join("src");
+        let expected = src.join("lib.rs");
+
+        assert_eq!(find_file_upwards(src, "lib.rs").unwrap(), expected);
+    }
+    #[test]
+    fn test_find_config_toml() {
+        // Expects to find our own Cargo.toml file.  Tests run from the root directory
+        let cwd = current_dir().expect("expected a working current_directory");
+        println!("cwd is {:?}", cwd);
+        let expected = cwd.join("Cargo.toml");
+        assert_eq!(find_config_toml(Some(cwd)).unwrap(), expected);
+    }
+}
